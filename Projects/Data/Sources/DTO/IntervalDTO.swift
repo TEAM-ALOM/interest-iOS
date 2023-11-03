70505c2eb3545fb9c0f0f24bf7e4bbcbb15bfb1c
송영모 <mooyoung2309@gmail.com>
2023-11-03 16:48:08 +0900

    feat: interval feature 세분화

---
+++ Projects/Data/Sources/DTO/IntervalDTO.swift
@@ -0,0 +1,8 @@
+//
+//  IntervalDTO.swift
+//  Data
+//
+//  Created by 송영모 on 11/3/23.
+//
+
+import Foundation
--- Projects/WatchFeature/Sources/tmp.swift
+++
@@ -1,8 +0,0 @@
-//
-//  tmp.swift
-//  interest-iOSManifests
-//
-//  Created by 송영모 on 11/2/23.
-//
-
-import Foundation
