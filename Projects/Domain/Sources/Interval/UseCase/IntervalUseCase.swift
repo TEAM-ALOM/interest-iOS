fe9792c1a102f7de555a634d0899ae29aeccbdf7
송영모 <mooyoung2309@gmail.com>
2023-11-29 16:13:38 +0900

    feat: usecase에 추가

--- Projects/Domain/Sources/Interval/UseCase/IntervalUseCase.swift
+++ Projects/Domain/Sources/Interval/UseCase/IntervalUseCase.swift
@@ -9,7 +9,11 @@ import Foundation
 import Combine
 
 public protocol IntervalUseCaseInterface {
+    func fetchInterval(id: UUID) -> IntervalEntity?
     func fetchIntervals() -> [IntervalEntity]
+    func saveInterval(interval: IntervalEntity) -> IntervalEntity
+    func updateInterval(at id: UUID, to interval: IntervalEntity) -> IntervalEntity?
+    func deleteInterval(at id: UUID) -> Bool
 }
 
 public final class IntervalUseCase: IntervalUseCaseInterface {
@@ -19,7 +23,23 @@ public final class IntervalUseCase: IntervalUseCaseInterface {
         self.intervalRepository = intervalRepository
     }
     
+    public func fetchInterval(id: UUID) -> IntervalEntity? {
+        return intervalRepository.fetch(id: id)
+    }
+    
     public func fetchIntervals() -> [IntervalEntity] {
         return intervalRepository.fetches()
+    }
+    
+    public func saveInterval(interval: IntervalEntity) -> IntervalEntity {
+        return intervalRepository.save(interval: interval)
+    }
+    
+    public func updateInterval(at id: UUID, to interval: IntervalEntity) -> IntervalEntity? {
+        return intervalRepository.update(at: id, to: interval)
+    }
+    
+    public func deleteInterval(at id: UUID) -> Bool {
+        return intervalRepository.delete(at: id)
     }
 }
